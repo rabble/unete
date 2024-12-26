e9fea27a1e949a69dad2d804c19c073a1edf1548
rabble <evan@protest.net>
2024-12-27 11:04:23 +1300

    feat: Add ParaglideJS internationalization wrapper to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
+  import { i18n } from "$lib/i18n";
+  import { ParaglideJS } from "@inlang/paraglide-sveltekit";
   import '../app.css';
 </script>
 
-<slot />
+<ParaglideJS {i18n}>
+	<slot></slot>
+</ParaglideJS>
